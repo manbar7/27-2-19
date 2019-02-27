a42cd8073d0bad9bc5d4f9ec6cb3b7865aeabaaa
manbar7 <45047021+manbar7@users.noreply.github.com>
2019-02-27 17:03:30 +0200

    Add files via upload

---
+++ ConsoleApp22/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+     class Car
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        private int Codan;
+        protected int NumberOfSeats;
+
+        public Car(string model, string brand, int year, string color)
+        {
+            Model = model;
+            Brand = brand;
+            Year = year;
+            Color = color;
+        }
+
+        public Car(string model, string brand, int year, string color, int codan, int numberOfSeats) : this(model, brand, year, color)
+        {
+            Codan = codan;
+            NumberOfSeats = numberOfSeats;
+        }
+
+       public int GetCodan()
+        {
+            return Codan;
+        }
+        public int GetNumberOfSeats()
+        {
+            return NumberOfSeats;
+        }
+        protected void ChangeNumberOfSeats(int Number)
+        {
+            return;
+        }
+
+    }
+
+}
